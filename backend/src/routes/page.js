0db38fd0bd016c0bf9c2cf20423e320e3b0850fa
MatteoArna <matteo.arena711@gmail.com>
2024-07-20 11:18:53 +0200

    Added route for page

--- backend/src/routes/page.js
+++ backend/src/routes/page.js
@@ -2,3 +2,8 @@ const PageController = require('../controllers/pageController');
 const createBaseRouter = require('./baseRoute');
 
 const router = createBaseRouter(PageController);
+
+// Rotte GET aggiuntive
+router.get('/type/:typeId', (req, res) => PageController.getPageByType(req, res));
+
+module.exports = router;
