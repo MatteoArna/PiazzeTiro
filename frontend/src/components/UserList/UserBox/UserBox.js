e22604da2101462c8e535073dca6c995a5931694
MatteoArna <matteo.arena711@gmail.com>
2024-07-30 12:53:55 +0200

    Color support for user status

--- frontend/src/components/UserList/UserBox/UserBox.js
+++ frontend/src/components/UserList/UserBox/UserBox.js
@@ -2,14 +2,31 @@ import React from 'react';
 import './UserBox.css';
 
 const UserBox = ({ userData }) => {
+
+  const translateStatus = (status) => {
+    switch (status) {
+      case 0:
+        return 'waiting documents';
+      case 1:
+        return 'pending';
+      case 2:
+        return 'accepted';
+      case 3:
+        return 'rejected';
+      default:
+        return 'unknown';
+    }
+  };
+
+
   return (
     <div className="user-box">
       <div className="user-info">
       <div className="user-company">{userData.society}</div>
       <div className="user-name">{userData.firstName + " " + userData.lastName}</div>
       </div>
-      <div className={`user-status ${userData.status <= 1 ? 'pending' : 'available'}`}>
-        {userData.status}
+      <div className={`user-status ${userData.status <= 1 ? 'pending' : (userData.status > 2 ? 'rejected' : 'accepted')}`}>
+        {translateStatus(userData.status)}
       </div>
       <div className="user-arrow">›</div>
     </div>
