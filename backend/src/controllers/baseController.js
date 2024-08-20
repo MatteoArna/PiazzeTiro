47b49cf168ee0959c87c468364e7a6b1d798d57b
MatteoArna <matteo.arena711@gmail.com>
2024-08-20 12:10:19 +0200

    E-mail for reservation

--- backend/src/controllers/baseController.js
+++ backend/src/controllers/baseController.js
@@ -1,14 +1,47 @@
+const nodemailer = require('nodemailer');
 const { Op } = require('sequelize');
 
 class BaseController {
     constructor(model) {
       this.model = model;
+
+      //Configurazione e-mail
+      this.transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: 'graciela.watsica@ethereal.email', 
+            pass: 'WXwsCgvhFChSefKCuH' // Sostituisci con le tue credenziali Ethereal
+        }
+      });
     }
   
     // Metodo per creare una nuova istanza
     async create(req, res) {
       try {
         const item = await this.model.create(req.body);
+
+        // Verifica se l'elemento creato è un booking, in base a un attributo specifico
+        if (this.model.name === 'Booking') { // Supponendo che il modello si chiami 'Booking'
+          // Configura l'email da inviare
+          const mailOptions = {
+              from: '"Booking Notification" <no-reply@example.com>', // Indirizzo fittizio
+              to: req.body.idCustomer, // Assumendo che l'email dell'utente sia fornita nel body della richiesta
+              subject: 'Conferma Prenotazione',
+              text: `Grazie per aver effettuato una prenotazione! Il tuo booking ID è: ${item.id}`,
+              html: `<b>Grazie per aver effettuato una prenotazione!</b><br>Il tuo booking ID è: ${item.id}`
+          };
+
+          // Invia l'email
+          this.transporter.sendMail(mailOptions, (error, info) => {
+              if (error) {
+                  console.error('Errore durante l\'invio dell\'email:', error);
+              } else {
+                  console.log('Email inviata:', nodemailer.getTestMessageUrl(info));
+              }
+          });
+        }
+
         res.status(201).json(item);
       } catch (error) {
         res.status(500).json({ message: 'Internal server error', error });
